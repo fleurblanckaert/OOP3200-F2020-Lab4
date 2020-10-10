8de3f1d2b6ac095350e12406dfb1873679fdb62b
fleurblanckaert <60987478+fleurblanckaert@users.noreply.github.com>
2020-10-10 03:56:52 -0400

    Add project files.

---
+++ OOP3200-F2020-Lab4/StandardDeck.cpp
@@ -0,0 +1,33 @@
+//Name: Fleur Blanckaert (100747083), Gabriel Dietrich 
+
+#include "StandardDeck.h"
+
+void StandardDeck::Initialize()
+{
+
+}
+
+StandardDeck::~StandardDeck()
+	= default;
+
+StandardDeck& StandardDeck::operator=(const StandardDeck & other_deck)
+{
+	// TODO: insert return statement here
+}
+
+int StandardDeck::CardsRemaining()
+{
+	return 0;
+}
+
+void StandardDeck::DrawNextCard()
+{
+}
+
+void StandardDeck::DrawRandomCard()
+{
+}
+
+void StandardDeck::Shuffle()
+{
+}
